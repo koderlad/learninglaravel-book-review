d1fa3f6e4eb2989f03bdebe69bb0760dd91e877c
Kamlesh Shrestha <thekamlesh@Kamleshs-MacBook-Pro.local>
2024-07-27 22:26:08 +0545

    Added Local Query Scopes

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -9,6 +9,8 @@ class Review extends Model
 {
     use HasFactory;
 
+    protected $fillable = ['review', 'rating'];
+
     //Inverse Side
     public function book(){
         return $this->belongsTo(Book::class);
